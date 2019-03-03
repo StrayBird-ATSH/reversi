364a46fcae16caa4d33a4ad04346b515be29a29e
Wang, Chen <zhanghuilian-98022@163.com>
2019-03-03 20:43:04 +0800

    A more revised version

--- src/Main.java
+++ src/Main.java
@@ -39,7 +39,27 @@ public class Main {
         }
     }
 
-    public static void placePiece() {
+    public static void placePiece(Board board) {
+        Color userColor = board.getUserColor();
+        int column = 0;
+        int row = 0;
+        int currentScore = 0;
+        int calculatedScore = 0;
+        for (int i = 0; i < board.size; i++)
+            for (int j = 0; j < board.size; j++) {
+                calculatedScore = calculateScore(board, row, column);
+                if (calculatedScore > currentScore) {
+                    row = i;
+                    column = j;
+                    currentScore = calculatedScore;
+                }
+            }
+        if (currentScore == 0) {
+            System.out.println("Both players have no valid move.\nGame over.\n");
+        }
+    }
 
+    public static int calculateScore(Board board, int row, int column) {
+        return 0;
     }
 }

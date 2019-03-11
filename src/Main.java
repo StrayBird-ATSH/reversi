a24efd053e3a8eaf548ce7478de77d9422dcf1b2
Chen Wang <zhanghuilian-98022@163.com>
2019-03-11 20:42:16 +0800

    A slight modification on the entrance of the method.

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to the Reversi game!\n" +
                 "Enter the board dimension:");
-        int size = input.nextInt();
+        int size = Integer.parseInt(input.nextLine());
         if (size % 2 != 0 || size > 11) {
             System.out.println("The size should be even and less than 10.");
             System.exit(0);
@@ -28,7 +28,7 @@ public class Main {
             System.exit(0);
         }
         boolean computerTurn = !(board.getUserColor() == Color.BLACK);
-        while (board.userCount + board.comCount < board.size * board.size &&
+        while (board.userCount + board.comCount == 0 || board.userCount + board.comCount < board.size * board.size &&
                 board.comCount != 0 && board.userCount != 0) {
             if (computerTurn) {
                 if (computer.placeable(board)) computer.place(board);

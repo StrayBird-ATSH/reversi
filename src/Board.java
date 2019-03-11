629cd13d077b7a0fde3b96f3fa0dbc61d0cd60d6
Chen Wang <zhanghuilian-98022@163.com>
2019-03-11 08:25:05 +0800

    The place-able method is finished but still needs some organizing work now.

--- src/Board.java
+++ src/Board.java
@@ -26,10 +26,11 @@ public class Board {
 
     boolean placeable(Color color) {
         Color inverseColor = color == Color.BLACK ? Color.WHITE : Color.BLACK;
-        ArrayList<Color> colorSequence = new ArrayList<>();
-        Color currentColor = null;
+        ArrayList<Color> colorSequence;
+        Color currentColor;
 //        In row direction
         for (int i = 0; i < size; i++) {
+            colorSequence = new ArrayList<>();
             for (int j = 0; j < size; j++) {
                 if (pieces[i][j] == null) currentColor = null;
                 else currentColor = pieces[i][j].color;
@@ -42,13 +43,95 @@ public class Board {
                                     colorSequence.get(colorSequence.size() - 3) == null)) return true;
                 }
             }
-            colorSequence = new ArrayList<>();
         }
 //        In column direction
+        for (int j = 0; j < size; j++) {
+            colorSequence = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                if (pieces[i][j] == null) currentColor = null;
+                else currentColor = pieces[i][j].color;
+                if (colorSequence.size() == 0) colorSequence.add(currentColor);
+                else if (colorSequence.get(colorSequence.size() - 1) != currentColor) {
+                    colorSequence.add(currentColor);
+                    if (colorSequence.size() >= 3 &&
+                            colorSequence.get(colorSequence.size() - 2) == inverseColor &&
+                            !(colorSequence.get(colorSequence.size() - 1) == null &&
+                                    colorSequence.get(colorSequence.size() - 3) == null)) return true;
+                }
+            }
+        }
 //        In diagonal direction
+        for (int j = 0; j < size; j++) {
+            int column = j;
+            colorSequence = new ArrayList<>();
+            for (int i = 0; i < size && column < size; i++, column++) {
+                if (pieces[i][column] == null) currentColor = null;
+                else currentColor = pieces[i][column].color;
+                if (colorSequence.size() == 0) colorSequence.add(currentColor);
+                else if (colorSequence.get(colorSequence.size() - 1) != currentColor) {
+                    colorSequence.add(currentColor);
+                    if (colorSequence.size() >= 3 &&
+                            colorSequence.get(colorSequence.size() - 2) == inverseColor &&
+                            !(colorSequence.get(colorSequence.size() - 1) == null &&
+                                    colorSequence.get(colorSequence.size() - 3) == null)) return true;
+                }
+            }
+        }
+
+        for (int i = 1; i < size; i++) {
+            int row = i;
+            colorSequence = new ArrayList<>();
+            for (int j = 0; j < size && row < size; j++, row++) {
+                if (pieces[row][j] == null) currentColor = null;
+                else currentColor = pieces[row][j].color;
+                if (colorSequence.size() == 0) colorSequence.add(currentColor);
+                else if (colorSequence.get(colorSequence.size() - 1) != currentColor) {
+                    colorSequence.add(currentColor);
+                    if (colorSequence.size() >= 3 &&
+                            colorSequence.get(colorSequence.size() - 2) == inverseColor &&
+                            !(colorSequence.get(colorSequence.size() - 1) == null &&
+                                    colorSequence.get(colorSequence.size() - 3) == null)) return true;
+                }
+            }
+        }
+
+
 //        In counter-diagonal direction
 
+        for (int i = 0; i < size; i++) {
+            int row = i;
+            colorSequence = new ArrayList<>();
+            for (int j = 0; j < size && row >= 0; j++, row--) {
+                if (pieces[row][j] == null) currentColor = null;
+                else currentColor = pieces[row][j].color;
+                if (colorSequence.size() == 0) colorSequence.add(currentColor);
+                else if (colorSequence.get(colorSequence.size() - 1) != currentColor) {
+                    colorSequence.add(currentColor);
+                    if (colorSequence.size() >= 3 &&
+                            colorSequence.get(colorSequence.size() - 2) == inverseColor &&
+                            !(colorSequence.get(colorSequence.size() - 1) == null &&
+                                    colorSequence.get(colorSequence.size() - 3) == null)) return true;
+                }
+            }
+        }
 
+
+        for (int j = 1; j < size; j++) {
+            int column = j;
+            colorSequence = new ArrayList<>();
+            for (int i = size - 1; i >= 0 && column < size; i--, column++) {
+                if (pieces[i][column] == null) currentColor = null;
+                else currentColor = pieces[i][column].color;
+                if (colorSequence.size() == 0) colorSequence.add(currentColor);
+                else if (colorSequence.get(colorSequence.size() - 1) != currentColor) {
+                    colorSequence.add(currentColor);
+                    if (colorSequence.size() >= 3 &&
+                            colorSequence.get(colorSequence.size() - 2) == inverseColor &&
+                            !(colorSequence.get(colorSequence.size() - 1) == null &&
+                                    colorSequence.get(colorSequence.size() - 3) == null)) return true;
+                }
+            }
+        }
         return false;
     }
 
